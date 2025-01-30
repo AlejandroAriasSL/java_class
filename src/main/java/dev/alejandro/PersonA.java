185a13fba8cd2a3be038347c36a6281d4364fa26
AlejandroAriasSL <alejandroasilva02@gmail>
2025-01-30 13:23:50 +0100

    3rd test completed

--- src/main/java/dev/alejandro/PersonA.java
+++ src/main/java/dev/alejandro/PersonA.java
@@ -4,12 +4,19 @@ public class PersonA {
 
     private String name;
 
-    public PersonA(String name) {
+    private String DNI;
+
+    public PersonA(String name, String DNI) {
         this.name = name;
+        this.DNI = DNI;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getDNI() {
+        return this.DNI;
     }
 
 }
--- src/test/java/dev/alejandro/JavaClassTest.java
+++ src/test/java/dev/alejandro/JavaClassTest.java
@@ -14,7 +14,7 @@ public class JavaClassTest {
     @DisplayName("Class PersonA is a class")
     public void test_person_is_a_class() {
 
-        PersonA person = new PersonA("gato");
+        PersonA person = new PersonA("gato", "33333333D");
 
         assertThat(person, instanceOf(PersonA.class));
     }
@@ -22,7 +22,7 @@ public class JavaClassTest {
     @DisplayName("Class PersonA name is a string")
     public void test_person_name_is_string() {
 
-        PersonA person = new PersonA("alejandro");
+        PersonA person = new PersonA("alejandro", "33333333D");
 
         assertThat(person.getName(), is(instanceOf(String.class)));
     }
@@ -30,7 +30,7 @@ public class JavaClassTest {
     @DisplayName("Class PersonA DNI is a string")
     public void test_person_dni_is_string() {
 
-        PersonA person = new PersonA("33333333D");
+        PersonA person = new PersonA("gato","33333333D");
 
         assertThat(person.getDNI(), is(instanceOf(String.class)));
     }
